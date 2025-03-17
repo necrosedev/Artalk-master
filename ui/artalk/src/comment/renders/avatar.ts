ec3a6a325b307d72a7f70fe53aef38c5ebf3cdd8
Necrose <167679361+necrosedev@users.noreply.github.com>
2025-03-17 07:38:58 +0700

    sdsd

--- ui/artalk/src/comment/renders/avatar.ts
+++ ui/artalk/src/comment/renders/avatar.ts
@@ -5,6 +5,7 @@ import type Render from '../render'
  * 评论头像界面
  */
 export default function renderAvatar(r: Render) {
+  
   /*const $avatar = r.$el.querySelector<HTMLElement>('.atk-avatar')!
   const $avatarImg = Utils.createElement<HTMLImageElement>('<img />')
 
@@ -21,4 +22,12 @@ export default function renderAvatar(r: Render) {
   } else {
     $avatar.append($avatarImg)
   }*/
+
+    const $avatar = r.$el.querySelector<HTMLElement>('.atk-avatar')!
+
+    const initials: string = r.data.nick.split(' ').map(word => word.charAt(0)).join('').substring(0, 2).toUpperCase();
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    
+    $avatar.textContent = initials;
+    $avatar.style.backgroundColor = randomColor;
 }
