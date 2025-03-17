85503a326d5955e9d12befca1dac88199f1d7cf2
Necrose <167679361+necrosedev@users.noreply.github.com>
2025-03-17 07:42:33 +0700

    sdsd

--- ui/artalk/src/comment/renders/reply-at.ts
+++ ui/artalk/src/comment/renders/reply-at.ts
@@ -5,7 +5,7 @@ import type Render from '../render'
  * Reply user indicator (with arrow icon) in the comment meta header
  */
 export default function renderReplyAt(r: Render) {
-  if (r.opts.flatMode || r.data.rid === 0) return // if not nested mode or root comment
+  /*if (r.opts.flatMode || r.data.rid === 0) return // if not nested mode or root comment
   if (!r.opts.replyTo) return // if no replyTo data
 
   r.$replyAt = Utils.createElement(
@@ -16,5 +16,5 @@ export default function renderReplyAt(r: Render) {
     r.comment.getActions().goToReplyComment()
   }
 
-  r.$headerBadgeWrap.insertAdjacentElement('afterend', r.$replyAt)
+  r.$headerBadgeWrap.insertAdjacentElement('afterend', r.$replyAt)*/
 }
